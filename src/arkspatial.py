3e8e85d8838e82b6c1d9202c52405ad75fde22e2
John Layt <john@layt.net>
2016-04-13 12:44:39 +0100

    Add open properties to context menu if not ARK layer

--- src/arkspatial.py
+++ src/arkspatial.py
@@ -60,35 +60,62 @@ class LayerTreeMenu(QgsLayerTreeViewMenuProvider):
         QgsLayerTreeViewMenuProvider.__init__(self)
         self._project = project
         self._view = view
+
+        # Default actions
         self._zoomGroup = self._view.defaultActions().actionZoomToGroup(self._project.mapCanvas())
         self._zoomLayer = self._view.defaultActions().actionZoomToLayer(self._project.mapCanvas())
+        self._featureCount = self._view.defaultActions().actionShowFeatureCount(self._project.mapCanvas())
         self._remove = self._view.defaultActions().actionRemoveGroupOrLayer(self._project.mapCanvas())
         self._rename = self._view.defaultActions().actionRenameGroupOrLayer(self._project.mapCanvas())
+
+        # Custom actions
         self._removeDrawings = QAction('Remove All Drawings', view)
         self._removeDrawings.triggered.connect(self._project.clearDrawings)
+
+        self._openAttributes = QAction(Project.getThemeIcon('mActionOpenTable.svg'), project.tr('&Open Attribute Table'), view)
+        self._openAttributes.triggered.connect(self._showAttributeTable)
+
+        self._openProperties = QAction(project.tr('&Properties'), view)
+        self._openProperties.triggered.connect(self._showLayerProperties)
 
     def createContextMenu(self):
         if not self._view.currentNode():
             return None
         menu = QMenu()
-        if self._view.currentNode().nodeType() == QgsLayerTreeNode.NodeGroup:
+        node = self._view.currentNode()
+        if node.nodeType() == QgsLayerTreeNode.NodeGroup:
             menu.addAction(self._zoomGroup)
-            name = self._view.currentNode().name()
-            if not self._project.isArkGroup(name):
+            if not self._project.isArkGroup(node.name()):
                 menu.addAction(self._rename)
                 menu.addAction(self._remove)
-            if name == self._project.drawingsGroupName:
+            if node.name() == self._project.drawingsGroupName:
                 menu.addAction(self._removeDrawings)
-        else:
+        elif node.nodeType() == QgsLayerTreeNode.NodeLayer:
             menu.addAction(self._zoomLayer)
-            layerId = self._view.currentNode().layerId()
-            if not self._project.isArkLayer(layerId):
-                menu.addAction(self._rename)
-                menu.addAction(self._remove)
-            parent = self._view.currentNode().parent()
+            if node.layer().type() == QgsMapLayer.VectorLayer:
+                menu.addAction(self._featureCount)
+                menu.addAction(self._openAttributes)
+            layerId = node.layerId()
+            parent = node.parent()
             if parent.nodeType() == QgsLayerTreeNode.NodeGroup and parent.name() == self._project.drawingsGroupName:
                 menu.addAction(self._removeDrawings)
+            if not self._project.isArkLayer(layerId):
+                menu.addSeparator()
+                menu.addAction(self._rename)
+                menu.addAction(self._remove)
+                if not QgsProject.instance().layerIsEmbedded(layerId):
+                    menu.addAction(self._openProperties)
         return menu
+
+    def _showAttributeTable(self):
+        node = self._view.currentNode()
+        if node.nodeType() == QgsLayerTreeNode.NodeLayer and node.layer().type() == QgsMapLayer.VectorLayer:
+            self._project.iface.showAttributeTable(node.layer())
+
+    def _showLayerProperties(self):
+        node = self._view.currentNode()
+        if node.nodeType() == QgsLayerTreeNode.NodeLayer:
+            self._project.iface.showLayerProperties(node.layer())
 
 
 class ArkSpatial(Plugin):

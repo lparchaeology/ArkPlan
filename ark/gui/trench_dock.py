b0926287bb17f6739ef436fcc4a35ad60aeb1b69
John Layt <john@.layt.net>
2017-10-22 14:42:37 +0100

    Add missed files

---
+++ ark/gui/trench_dock.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                                ARK Spatial
+                    A QGIS plugin for Archaeological Recording.
+        Part of the Archaeological Recording Kit by L-P : Archaeology
+                        http://ark.lparchaeology.com
+                              -------------------
+        copyright            : 2017 by L-P : Heritage LLP
+        email                : ark@lparchaeology.com
+        copyright            : 2017 by John Layt
+        email                : john@layt.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+from PyQt4.QtCore import pyqtSignal
+
+from ArkSpatial.ark.lib.gui import ToolDockWidget
+
+from .trench_widget import TrenchWidget
+
+
+class TrenchDock(ToolDockWidget):
+
+    drawTrenchSelected = pyqtSignal()
+    generateTrenchSelected = pyqtSignal()
+    exportTrenchSelected = pyqtSignal()
+
+    sampleChanged = pyqtSignal()
+    sampleAreaChanged = pyqtSignal(str)
+    samplePercentChanged = pyqtSignal(float)
+    trenchWidthChanged = pyqtSignal(float)
+    trenchLengthChanged = pyqtSignal(float)
+
+    def __init__(self, parent=None):
+        super(TrenchDock, self).__init__(TrenchWidget(), parent)
+
+        self.setWindowTitle(u'ARK Trench')
+        self.setObjectName(u'TrenchDock')
+
+    def initGui(self, iface, location, menuAction):
+        super(TrenchDock, self).initGui(iface, location, menuAction)
+
+        self._drawTrenchAction = self.addToolbarAction(
+            ':/plugins/ark/trench/draw.svg',
+            'Draw Trench',
+            self.drawTrenchSelected
+        )
+
+        self._generateTrenchAction = self.addToolbarAction(
+            ':/plugins/ark/trench/generate.svg',
+            'Generate Trenches',
+            self.generateTrenchSelected
+        )
+
+        self._exportTrenchAction = self.addToolbarAction(
+            ':/plugins/ark/trench/export.svg',
+            'Export Trenches',
+            self.exportTrenchSelected
+        )
+
+        self.widget.areaCombo.currentIndexChanged.connect(self.sampleChanged)
+        self.widget.areaCombo.currentIndexChanged.connect(self._areaChanged)
+        self.widget.samplePercentSpin.valueChanged.connect(self.sampleChanged)
+        self.widget.samplePercentSpin.valueChanged.connect(self.samplePercentChanged)
+        self.widget.widthSpin.valueChanged.connect(self.sampleChanged)
+        self.widget.widthSpin.valueChanged.connect(self.trenchWidthChanged)
+        self.widget.lengthSpin.valueChanged.connect(self.sampleChanged)
+        self.widget.lengthSpin.valueChanged.connect(self.trenchLengthChanged)
+
+    def unloadGui(self):
+        pass
+
+    def loadProject(self, project):
+        self.areaCombo.clear()
+        for area in []:
+            self.areaCombo.addItem(area, area)
+
+    def closeProject(self):
+        pass
+
+    def setArea(self, id, size):
+        idx = self.areaCombo.findData(id)
+        if idx >= 0:
+            self.areaCombo.setCurrentIndex(idx)
+        self.widget.areaSpin.setValue(size)
+
+    def samplePercent(self):
+        return self.widget.samplePercentSpin.value()
+
+    def setSamplePercent(self, percent):
+        self.widget.samplePercentSpin.setValue(percent)
+
+    def trenchWidth(self):
+        return self.widget.widthSpin.value()
+
+    def setTrenchWidth(self, width):
+        self.widget.widthSpin.setValue(width)
+
+    def trenchLength(self):
+        return self.widget.lengthSpin.value()
+
+    def setTrenchLength(self, length):
+        self.widget.lengthSpin.setValue(length)
+
+    def setResult(self, count, length, area):
+        self.widget.sampleCountSpin.setValue(count)
+        self.widget.sampleLengthSpin.setValue(count)
+        self.widget.sampleAreaSpin.setValue(count)
+
+    def _areaChanged(self, idx):
+        self.sampleAreaChanged.emit(self.widget.areaCombo.itemData(idx))
---
+++ ark/trench_module.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+                                ARK Spatial
+                    A QGIS plugin for Archaeological Recording.
+        Part of the Archaeological Recording Kit by L-P : Archaeology
+                        http://ark.lparchaeology.com
+                              -------------------
+        copyright            : 2017 by L-P : Heritage LLP
+        email                : ark@lparchaeology.com
+        copyright            : 2017 by John Layt
+        email                : john@layt.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+import re
+
+from PyQt4.QtCore import QObject, Qt
+
+from ArkSpatial.ark.lib.core import layers
+
+from ArkSpatial.ark.gui import TrenchDock
+
+
+class TrenchModule(QObject):
+
+    project = None  # Project()
+
+    # Internal variables
+    dock = None  # FilterDock()
+    _initialised = False
+
+    def __init__(self, project):
+        super(TrenchModule, self).__init__(project)
+        self.project = project
+
+    # Standard Dock methods
+
+    # Create the gui when the plugin is first created
+    def initGui(self):
+        self.dock = TrenchDock(self.project.layerDock)
+        action = self.project.addDockAction(
+            ':/plugins/ark/filter/filter.png',
+            self.tr(u'Trench Tools'),
+            callback=self.run,
+            checkable=True
+        )
+        self.dock.initGui(self.project.iface, Qt.RightDockWidgetArea, action)
+
+    # Load the project settings when project is loaded
+    def loadProject(self):
+        return self._initialised
+
+    # Save the project
+    def writeProject(self):
+        pass
+
+    # Close the project
+    def closeProject(self):
+        # Reset the initialisation
+        self._initialised = False
+
+    # Unload the gui when the plugin is unloaded
+    def unloadGui(self):
+        self.dock.unloadGui()
+
+    def run(self, checked):
+        if checked and not self._initialised:
+            self.dock.menuAction().setChecked(False)
+
+    def showDock(self, show=True):
+        self.dock.menuAction().setChecked(show)
